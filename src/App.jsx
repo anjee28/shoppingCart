c0638cdf99f5041cee18e5292a76f659f12404d3
Mellhabib <anjee28.twoeight@gmail.com>
2022-11-26 21:05:58 +0800

    Fix cart total quantity counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,253 +1,256 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import uniqid from 'uniqid';
-import Cart from './pages/Cart';
-import Home from './pages/Home';
-import ErrorPage from './pages/ErrorPage';
-import CompComponent from './pages/Comp/CompComponent';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import uniqid from "uniqid";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import ErrorPage from "./pages/ErrorPage";
+import CompComponent from "./pages/Comp/CompComponent";
+import { HashRouter } from "react-router-dom";
 //data
-import { products } from './data';
+import { products } from "./data";
 
 function App() {
-	const [cart, setCart] = useState([]);
-	const [cartTotal, setCartTotal] = useState(0);
-	useEffect(() => {
-		if (sessionStorage.length !== 0) {
-			const cartSession = JSON.parse(sessionStorage.cart);
-			setCart(cartSession);
-			setCartTotal(sessionStorage.total);
-		}
-	}, []);
+  const [cart, setCart] = useState([]);
+  const [cartTotal, setCartTotal] = useState(0);
+  useEffect(() => {
+    if (sessionStorage.length !== 0) {
+      const cartSession = JSON.parse(sessionStorage.cart);
+      setCart(cartSession);
+      setCartTotal(sessionStorage.total);
+    }
+  }, []);
 
-	const [category, setCategory] = useState([
-		{
-			id: 0,
-			name: 'All Products',
-			url: '/components',
-			data: products,
-		},
-		{
-			id: 1,
-			name: 'Processors',
-			url: '/components-cpu',
-			data: products.filter(function (product) {
-				return product.category === 'cpu';
-			}),
-		},
-		{
-			id: 2,
-			name: 'Motherbaords',
-			url: '/components-motheboard',
-			data: products.filter(function (product) {
-				return product.category === 'mboard';
-			}),
-		},
-		{
-			id: 3,
-			name: 'RAM',
-			url: '/components-ram',
-			data: products.filter(function (product) {
-				return product.category === 'ram';
-			}),
-		},
-		/*{ id: 3, name: 'PSU', url: '/components-psu', data: [] },
+  const [category, setCategory] = useState([
+    {
+      id: 0,
+      name: "All Products",
+      url: "/components",
+      data: products,
+    },
+    {
+      id: 1,
+      name: "Processors",
+      url: "/components-cpu",
+      data: products.filter(function (product) {
+        return product.category === "cpu";
+      }),
+    },
+    {
+      id: 2,
+      name: "Motherbaords",
+      url: "/components-motheboard",
+      data: products.filter(function (product) {
+        return product.category === "mboard";
+      }),
+    },
+    {
+      id: 3,
+      name: "RAM",
+      url: "/components-ram",
+      data: products.filter(function (product) {
+        return product.category === "ram";
+      }),
+    },
+    /*{ id: 3, name: 'PSU', url: '/components-psu', data: [] },
 		{ id: 4, name: 'Storage', url: '/components-storage', data: [] },
 		{ id: 5, name: 'Graphics Card', url: '/components-gpu', data: [] },
 		{ id: 6, name: 'Chassis', url: '/components-chassis', data: [] }, */
-	]);
+  ]);
 
-	//Add to cart Function
-	const addToCart = (id) => {
-		const cartTemp = [...cart];
+  //Add to cart Function
+  const addToCart = (id) => {
+    const cartTemp = [...cart];
 
-		const indexCart = cartTemp.findIndex((object) => {
-			return object.productId === parseInt(id);
-		});
+    const indexCart = cartTemp.findIndex((object) => {
+      return object.productId === parseInt(id);
+    });
 
-		//find the index of the product from products data using id
-		const indexItem = products.findIndex((object) => {
-			return object.id === parseInt(id);
-		});
+    //find the index of the product from products data using id
+    const indexItem = products.findIndex((object) => {
+      return object.id === parseInt(id);
+    });
 
-		//item is the product that matches the id
-		const item = products[indexItem];
-		console.log(id);
+    //item is the product that matches the id
+    const item = products[indexItem];
+    console.log(id);
 
-		if (cartTemp[indexCart] === undefined) {
-			//creats new entry to the cart
-			const newItem = {
-				id: uniqid(),
-				productId: item.id,
-				brand: item.brand,
-				model: item.model,
-				price: item.price,
-				thumb: item.thumb,
-				quantity: 1,
-			};
+    if (cartTemp[indexCart] === undefined) {
+      //creats new entry to the cart
+      const newItem = {
+        id: uniqid(),
+        productId: item.id,
+        brand: item.brand,
+        model: item.model,
+        price: item.price,
+        thumb: item.thumb,
+        quantity: 1,
+      };
 
-			cartTemp.push(newItem); //adds the new entry to the cart
-			setCart(cartTemp);
-			sessionStorage.setItem('cart', JSON.stringify(cartTemp));
+      cartTemp.push(newItem); //adds the new entry to the cart
+      setCart(cartTemp);
+      sessionStorage.setItem("cart", JSON.stringify(cartTemp));
 
-			cartSum(cartTemp); //computes the total amount of the cart
-		} else {
-			addQuantity(id);
-		}
-	};
+      cartSum(cartTemp); //computes the total amount of the cart
+    } else {
+      addQuantity(id);
+    }
+  };
 
-	const cartSum = (cartTemp) => {
-		const itemPrices = [];
-		let finalTotal;
-		cartTemp.forEach((prices) => {
-			itemPrices.push(prices.price * prices.quantity);
-		});
+  const cartSum = (cartTemp) => {
+    const itemPrices = [];
+    let finalTotal;
+    cartTemp.forEach((prices) => {
+      itemPrices.push(prices.price * prices.quantity);
+    });
 
-		let sum = (...numbers) => {
-			return numbers.reduce((prev, current) => prev + current);
-		};
+    let sum = (...numbers) => {
+      return numbers.reduce((prev, current) => prev + current);
+    };
 
-		if (itemPrices.length === 0) {
-			finalTotal = 0;
-		} else {
-			finalTotal = Math.round(sum(...itemPrices) * 100) / 100;
-		}
+    if (itemPrices.length === 0) {
+      finalTotal = 0;
+    } else {
+      finalTotal = Math.round(sum(...itemPrices) * 100) / 100;
+    }
 
-		setCartTotal(parseFloat(finalTotal));
-		sessionStorage.setItem('total', finalTotal);
-	};
+    setCartTotal(parseFloat(finalTotal));
+    sessionStorage.setItem("total", finalTotal);
+  };
 
-	const addQuantity = (id) => {
-		const cartTemp = [...cart];
-		const productId = id;
-		console.log(cartTemp);
-		console.log(productId);
-		const index = cartTemp.findIndex((object) => {
-			return object.productId === parseInt(productId);
-		});
-		cartTemp[index].quantity++;
-		setCart(cartTemp);
-		sessionStorage.setItem('cart', JSON.stringify(cartTemp));
-		cartSum(cartTemp);
-	};
+  const addQuantity = (id) => {
+    const cartTemp = [...cart];
+    const productId = id;
+    console.log(cartTemp);
+    console.log(productId);
+    const index = cartTemp.findIndex((object) => {
+      return object.productId === parseInt(productId);
+    });
+    cartTemp[index].quantity++;
+    setCart(cartTemp);
+    sessionStorage.setItem("cart", JSON.stringify(cartTemp));
+    cartSum(cartTemp);
+  };
 
-	const subtractQuantity = (e) => {
-		const cartTemp = [...cart];
-		const id = e.target.id;
-		const index = cartTemp.findIndex((object) => {
-			return object.id === id;
-		});
-		if (cartTemp[index].quantity === 1) {
-			deleteEntry(e);
-		} else {
-			cartTemp[index].quantity--;
-			setCart(cartTemp);
-			sessionStorage.setItem('cart', JSON.stringify(cartTemp));
-			cartSum(cartTemp);
-		}
-	};
+  const subtractQuantity = (e) => {
+    const cartTemp = [...cart];
+    const id = e.target.id;
+    const index = cartTemp.findIndex((object) => {
+      return object.id === id;
+    });
+    if (cartTemp[index].quantity === 1) {
+      deleteEntry(e);
+    } else {
+      cartTemp[index].quantity--;
+      setCart(cartTemp);
+      sessionStorage.setItem("cart", JSON.stringify(cartTemp));
+      cartSum(cartTemp);
+    }
+  };
 
-	const deleteEntry = (e) => {
-		const cartTemp = [...cart];
-		const id = e.target.id;
-		const index = cartTemp.findIndex((object) => {
-			return object.id === id;
-		});
-		cartTemp.splice(index, 1);
-		setCart(cartTemp);
-		sessionStorage.setItem('cart', JSON.stringify(cartTemp));
-		cartSum(cartTemp);
-	};
+  const deleteEntry = (e) => {
+    const cartTemp = [...cart];
+    const id = e.target.id;
+    const index = cartTemp.findIndex((object) => {
+      return object.id === id;
+    });
+    cartTemp.splice(index, 1);
+    setCart(cartTemp);
+    sessionStorage.setItem("cart", JSON.stringify(cartTemp));
+    cartSum(cartTemp);
+  };
 
-	return (
-		<React.Fragment>
-			<Router>
-				<nav>
-					<div className="logo">
-						<div
-							className="home"
-							onClick={(event) => (window.location.href = './')}
-						>
-							<svg idth="32px" height="32px" viewBox="0 0 24 24">
-								<path
-									fill="currentColor"
-									d="M6,2C4.89,2 4,2.89 4,4V12C4,13.11 4.89,14 6,14H18C19.11,14 20,13.11 20,12V4C20,2.89 19.11,2 18,2H6M6,4H18V12H6V4M4,15C2.89,15 2,15.89 2,17V20C2,21.11 2.89,22 4,22H20C21.11,22 22,21.11 22,20V17C22,15.89 21.11,15 20,15H4M8,17H20V20H8V17M9,17.75V19.25H13V17.75H9M15,17.75V19.25H19V17.75H15Z"
-								/>
-							</svg>
-							<span>ezPC</span>
-						</div>
-					</div>
-					<div className="navs">
-						<div className="navLinks">
-							<div
-								className="components"
-								onClick={(event) =>
-									(window.location.href = './components')
-								}
-							>
-								<span>Components</span>
-							</div>
-							<div
-								className="components"
-								onClick={(event) =>
-									(window.location.href = './components')
-								}
-							>
-								<span>Peripherals</span>
-							</div>
-							<div
-								className="components"
-								onClick={(event) =>
-									(window.location.href = './components')
-								}
-							>
-								<span>Accessories</span>
-							</div>
-						</div>
-						<div
-							className="cart"
-							onClick={(event) =>
-								(window.location.href = './cart')
-							}
-						>
-							<span>Cart - {cart.length}</span>
-						</div>
-					</div>
-				</nav>
-				<div className="mainBody">
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route
-							path="/cart"
-							element={
-								<Cart
-									data={cart}
-									total={cartTotal}
-									add={addQuantity}
-									minus={subtractQuantity}
-									delete={deleteEntry}
-								/>
-							}
-						/>
-						{category.map((data) => (
-							<Route
-								key={data.id}
-								path={data.url}
-								element={
-									<CompComponent
-										data={data}
-										category={category}
-										addToCart={addToCart}
-									/>
-								}
-							/>
-						))}
-						<Route path="*" element={<ErrorPage />} />
-					</Routes>
-				</div>
-				<div className="bg-sky-500 h-[6vh]"></div>
-			</Router>
-		</React.Fragment>
-	);
+  const renderCartQty = () => {
+    let qty = 0;
+    if (cart.length > 0) {
+      for (let i = 0; i < cart.length; i++) {
+        qty = qty + cart[i].quantity;
+      }
+    }
+    return qty;
+  };
+
+  return (
+    <React.Fragment>
+      <HashRouter>
+        <nav>
+          <div className="logo">
+            <div
+              className="home"
+              onClick={(event) => (window.location.href = "./")}
+            >
+              <svg idth="32px" height="32px" viewBox="0 0 24 24">
+                <path
+                  fill="currentColor"
+                  d="M6,2C4.89,2 4,2.89 4,4V12C4,13.11 4.89,14 6,14H18C19.11,14 20,13.11 20,12V4C20,2.89 19.11,2 18,2H6M6,4H18V12H6V4M4,15C2.89,15 2,15.89 2,17V20C2,21.11 2.89,22 4,22H20C21.11,22 22,21.11 22,20V17C22,15.89 21.11,15 20,15H4M8,17H20V20H8V17M9,17.75V19.25H13V17.75H9M15,17.75V19.25H19V17.75H15Z"
+                />
+              </svg>
+              <span>ezPC</span>
+            </div>
+          </div>
+          <div className="navs">
+            <div className="navLinks">
+              <div
+                className="components"
+                onClick={(event) => (window.location.href = "./#/components")}
+              >
+                <span>Components</span>
+              </div>
+              <div
+                className="components"
+                onClick={(event) => (window.location.href = "./#/components")}
+              >
+                <span>Peripherals</span>
+              </div>
+              <div
+                className="components"
+                onClick={(event) => (window.location.href = "./#/components")}
+              >
+                <span>Accessories</span>
+              </div>
+            </div>
+            <div
+              className="cart"
+              onClick={(event) => (window.location.href = "./#/cart")}
+            >
+              <span>Cart - {renderCartQty()}</span>
+            </div>
+          </div>
+        </nav>
+        <div className="mainBody">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/cart"
+              element={
+                <Cart
+                  data={cart}
+                  total={cartTotal}
+                  add={addQuantity}
+                  minus={subtractQuantity}
+                  delete={deleteEntry}
+                />
+              }
+            />
+            {category.map((data) => (
+              <Route
+                key={data.id}
+                path={data.url}
+                element={
+                  <CompComponent
+                    data={data}
+                    category={category}
+                    addToCart={addToCart}
+                  />
+                }
+              />
+            ))}
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </div>
+        <div className="bg-sky-500 h-[6vh]"></div>
+      </HashRouter>
+    </React.Fragment>
+  );
 }
 
 export default App;
--- src/pages/Comp/Card.jsx
+++ src/pages/Comp/Card.jsx
@@ -1,31 +1,27 @@
-import React from 'react';
+import React from "react";
 
 function Card(props) {
-	return (
-		<div className=" bg-white p-2 flex flex-col w-52 h-[21rem] hover:shadow-lg hover:border">
-			<div className="grow">
-				<a href={props.data.url}>
-					<div className="">
-						<img src={props.data.thumb} alt={props.data.model} />
-					</div>
-					<div className="mt-1 text-left font-semibold">
-						{props.data.brand} {props.data.model}
-					</div>
-				</a>
-			</div>
-			<div>
-				<div className="text-left text-xl mb-2">
-					${props.data.price}
-				</div>
-			</div>
-			<div
-				className="bg-sky-500 text-white py-2 w-full hover:bg-sky-300 cursor-pointer"
-				onClick={() => props.addToCart(props.data.id)}
-			>
-				Add to Cart
-			</div>
-		</div>
-	);
+  return (
+    <div className=" bg-white p-2 flex flex-col w-52 h-[21rem] hover:shadow-lg hover:border">
+      <div className="grow">
+        <div className="">
+          <img src={props.data.thumb} alt={props.data.model} />
+        </div>
+        <div className="mt-1 text-left font-semibold">
+          {props.data.brand} {props.data.model}
+        </div>
+      </div>
+      <div>
+        <div className="text-left text-xl mb-2">${props.data.price}</div>
+      </div>
+      <div
+        className="bg-sky-500 text-white py-2 w-full hover:bg-sky-300 cursor-pointer"
+        onClick={() => props.addToCart(props.data.id)}
+      >
+        Add to Cart
+      </div>
+    </div>
+  );
 }
 
 export default Card;
